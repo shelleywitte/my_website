27cdc6d51a6ecd640e008a64fa83e0d064a1d23d
shelleywitte <switte@wisc.edu>
2016-01-27 17:10:22 -0600

    main.js work
    cities created, working on table creation

--- js/main.js
+++ js/main.js
@@ -1,1 +1,34 @@
 /* Created by Shelley Witte, 2016 */
+
+$('#mydiv').html('Hello World!');
+
+function initialize() {
+    cities();
+};
+
+function cities() {
+    var cityPop = [
+    {
+        city: 'Minneapolis',
+        population: 400070
+    },
+    {
+        city: 'St. Paul',
+        population: 294873,
+    },
+    {
+        city: 'Washington, DC',
+        population: 658893
+    },
+    {
+        city: 'Stillwater',
+        population: 18674
+    }
+    ];
+
+    for (var i = 0; i < cityPop.length; i++) {
+    console.log(cityPop[i]);
+}
+};
+
+$(document).ready(initialize);
