08a287312d6a46028d929053ce441e1da0222216
shelleywitte <switte@wisc.edu>
2016-02-02 16:47:51 -0600

    random_color
    working on main.js for changing table color

--- js/main.js
+++ js/main.js
@@ -4,10 +4,15 @@ $('#mydiv').html('Hello World!');
 
 function initialize() {
     cities();
+    addColumns();
+    addEvents();
+
 };
 
+var cityPop;
+
 function cities() {
-    var cityPop = [
+    cityPop = [
     {
         city: 'Minneapolis',
         population: 400070
@@ -26,9 +31,95 @@ function cities() {
     }
     ];
 
-    for (var i = 0; i < cityPop.length; i++) {
-    console.log(cityPop[i]);
-}
+    $("#mydiv").append("<table>");
+
+    $("table").append("<tr>");
+
+    //adds "City" and "Population" columns to the header row
+    $("tr").append("<th>City</th><th>Population</th>");
+
+    //loop to add a new row for each city
+    for (var i = 0; i < cityPop.length; i++){
+        //assign longer html strings to a variable
+        var rowHtml = "<tr><td>" + cityPop[i].city + "</td><td>" + cityPop[i].population + "</td></tr>";
+        //add the row's html string to the table
+        $("table").append(rowHtml);
+    };
 };
 
+
+//Debugging section
+
+// defines a function that takes the cityPop global variable as a parameter 
+function addColumns(){
+    
+    // iterates over each row in the table 
+    $('tr').each(function(i){
+
+        if (i == 0){
+
+            // adds (appends) a third column to the table with the header labeled "City Size"
+            $(this).append('<th>City Size</th>');
+        } else {
+
+            //creates variable citySize 
+            var citySize;
+            
+            //if statement that looks at cityPop object population property and
+            //designates the city as small/medium/large according to the criteria
+            if (cityPop[i-1].population < 100000){
+                citySize = 'Small';
+
+            } else if (cityPop[i-1].population < 500000){
+                citySize = 'Medium';
+
+            } else {
+                citySize = 'Large';
+            };
+
+            //adds the city size designation as new table data to the same row 
+            //it assessed in the if statement
+            $(this).append('<td>' + citySize + '</td>');
+        };
+
+
+    });
+};
+
+
+function addEvents(){
+
+    $('table').mouseover(function(){
+        
+        var color = "rgb";
+
+        for (var i=0; i<3; i++){
+
+            var random = Math.round(Math.random() * 255);
+
+            color += "random";
+
+            if (i<2) {
+                color += ",";
+            
+            } else {
+                color += ")";
+        };
+    }
+
+        $(this).css('color', color);
+        
+    });
+
+};
+
+// adds click listener that will give an alert if the table is clicked on
+$('table').on('click', function(){
+    alert('Hey, you clicked me!');
+
+});
+
+
+
 $(document).ready(initialize);
+
